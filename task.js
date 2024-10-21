30c339facd8f95efa2d9474d2faa0a18bebcbc38
AshokMadagoni <madagoniashok1@gmail.com>
2024-10-21 22:12:03 +0530

    added

---
+++ task.js
@@ -0,0 +1,26 @@
+let str= "Hello World!";
+console.log(str.replace("World! ","My Dear Friend"))
+
+
+let a="Hungry  ";
+console.log(a.repeat(10));
+
+
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+console.log(sentence.lastIndexOf("fox"));
+
+
+// let message = "Hello, world!";
+// let arr=message.split(" ")
+// let result=arr.splice(0,0,"hi")
+// let final=arr.toString(" ")
+// console.log(final);
+
+let message = "Hello, world!";
+console.log(message.startsWith("hi"));
+
+
+let fileName = "document.pdf";
+console.log(fileName.endsWith(".pdf"));
+
+
